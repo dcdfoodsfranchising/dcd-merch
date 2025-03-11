fa760e69fefc59a9258060fb6ff6b7dadec57213
lorens00 <lansanglawrence3@gmail.com>
2025-03-11 15:59:53 -0700

    removed passport

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user");
-const passport = require("passport");
 const { verify, verifyAdmin } = require("../auth");
 
 // User Registration
