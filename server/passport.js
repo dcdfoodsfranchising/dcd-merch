2c7c6bb29b47be8ab93044c62f2c8e22ad39944c
lorens00 <lansanglawrence3@gmail.com>
2025-03-11 14:46:01 -0700

    passport 5

--- server/passport.js
+++ server/passport.js
@@ -1,43 +1,27 @@
+require("dotenv").config();
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const User = require("./Models/User"); 
 
 passport.use(
     new GoogleStrategy(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: process.env.GOOGLE_CALLBACK_URL
+            callbackURL: process.env.GOOGLE_CALLBACK_URL,
+            passReqToCallback: true
         },
-        async (accessToken, refreshToken, profile, done) => {
-            try {
-                let user = await User.findOne({ googleId: profile.id });
-                if (!user) {
-                    user = new User({
-                        googleId: profile.id,
-                        name: profile.displayName,
-                        email: profile.emails[0].value,
-                        avatar: profile.photos[0].value
-                    });
-                    await user.save();
-                }
-                return done(null, user);
-            } catch (err) {
-                return done(err, null);
-            }
+        function (request, accessToken, refreshToken, profile, done) {
+            return done(null, profile);
         }
     )
 );
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+    done(null, user);
 });
 
-passport.deserializeUser(async (id, done) => {
-    try {
-        const user = await User.findById(id);
-        done(null, user);
-    } catch (err) {
-        done(err, null);
-    }
+passport.deserializeUser((user, done) => {
+    done(null, user);
 });
+
+module.exports = passport;
